78f65ca58be6b372ee091c2438f719468487e709
EC2 Default User <ec2-user@ip-172-31-19-62.ec2.internal>
2021-01-23 06:20:54 +0000

    [finish] 2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -7,7 +7,15 @@ end
 def show
   @list = List.find(params[:id])
 end
+def edit
+  @list = List.find(params[:id])
+end
 
+def  update
+  list = List.find(params[:id])
+  list.update(list_params)
+  redirect_to todolist_path(list.id)
+end
 
   def new
     # Viewへ渡すためのインスタント変数にからのモデルオブジェクトを生成する。
